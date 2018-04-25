461fd559d5f0b28437fb95a49e95961390f10e01
pichayakul <pichayakul.j@ku.th>
2018-04-26 00:07:13 +0700

    merge

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::get('/userManager/setting/{user}', 'UsersManagerController@edit');
 Route::put('/userManager/update/{user}','UsersManagerController@update');
 
 
-Route::get('/parkings/addcarpark', 'ParkingsController@addcarpark');
+Route::get('/parkings/{parking}/addcarpark', 'ParkingsController@addcarpark');
 Route::get('/parkings/{parking}/edit','ParkingsController@edit');
 Route::get('/parkings', 'ParkingsController@index');
 Route::get('/parkings/create', 'ParkingsController@create');
@@ -47,6 +47,7 @@ Route::get('/parkings/{parking}','ParkingsController@show')->where('id','[0-9]+'
 Route::post('/parkings', 'ParkingsController@store');
 Route::put('/parkings/{parking}','ParkingsController@update')->where('id','[0-9]+');
 Route::delete('/parkings/{parking}','ParkingsController@destroy')->where('id','[0-9]+');
+
 
 
 
