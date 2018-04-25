9274b3fb7ab4d3ad222ca32ec1069e0f9a22e7e1
b5810451063 <sakchai.l@ku.th>
2018-04-25 19:32:16 +0700

    ..

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
+use App\User;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -26,6 +26,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('index-userManager',function($userObject){
+            $user = \Auth::user();
+
+            return $user->isSuperAdmin() ;
+        });
+
+        
     }
 }
