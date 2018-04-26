5d25ae5a78e0b8f5c42860c748457fd3d6966ae0
b5810451063 <sakchai.l@ku.th>
2018-04-27 01:01:53 +0700

    Gate destroy

--- app/Http/Controllers/UsersManagerController.php
+++ app/Http/Controllers/UsersManagerController.php
@@ -131,7 +131,7 @@ class UsersManagerController extends Controller
      */
     public function destroy(User $user)
     {
-
+      if (\Gate::allows('index-userManagers')){
         if($user->is_enabled == 1){
             $user->is_enabled = 0;
         }elseif($user->is_enabled==0){
@@ -144,6 +144,9 @@ class UsersManagerController extends Controller
         $log->description = $users[$log->id_user].' delete this user';
         $log->save();
         return redirect('/userManager');
+      }else{
+            return view('/denieViews.denie');
+        }
 
     }
     public function createOwner(){
