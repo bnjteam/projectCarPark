88499c0397df18afc46aee141d10e907540e84e5
titiku <titi.t@ku.th>
2018-04-28 17:25:23 +0700

    ball

---
+++ resources/views/park/complete.blade.php
@@ -0,0 +1,26 @@
+
+@extends('layouts.app')
+@section('content')
+<div class="container">
+  <div class="row justify-content-center ">
+    <div class="col-10">
+      <div class="card border-primary ">
+        <div class="card-header" style="text-align:center">Buy Package</div>
+        <div class="card-body">
+          <div class="" >
+            <div class="row justify-content-center">
+                <img src="/storage/check.gif" loop=1 alt="" style="padding:20px">
+            </div>
+            <h3 style="text-align:center">Success</h3>
+            <p class="card-text" style="text-align:center">you have buy package.</p>
+            <div class="" style="text-align:center">
+                <a href="/search" class="btn btn-primary" >Go to reserve</a>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
