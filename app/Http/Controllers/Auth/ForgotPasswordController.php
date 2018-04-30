a71f7eb3e7f86070f75b3e32fa6012fcd454f75c
b5810451063 <sakchai.l@ku.th>
2018-04-30 19:59:56 +0700

    แก้เมล

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -40,13 +40,11 @@ class ForgotPasswordController extends Controller
 
       $log = new Log();
       $users = User::all()->pluck('id','email');
-
       $log->id_user = $users[$request->email];
           $users = User::all()->pluck('name','email');
       $log->description = "Email :".$request->email. ' has request to reset password';
       $log->save();
         $this->validateEmail($request);
-
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
